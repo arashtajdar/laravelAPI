a55fb74007c01d54c193ccbbb375a77cba6f77e6
arashtajdar <arashtajdar@gmail.com>
2022-04-13 11:07:10 +0200

    Add products get route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/products',[ProductController::class,'index']);
+
 Route::post('/products',[ProductController::class,'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
