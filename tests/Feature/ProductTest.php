2520b5408bddafbc1ba806b0458a2d40c2819a07
arashtajdar <arashtajdar@gmail.com>
2022-04-13 11:58:59 +0200

    Create two tests for product store method

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -18,4 +18,21 @@ class ProductTest extends TestCase
         $response = $this->get('/api/products');
         $response->assertStatus(200);
     }
+
+    public function test_product_store_functionality()
+    {
+        // Test if we do not have any input for product store method, it will fail and respond with 422
+        $response = $this->post('/api/products');
+        $response->assertStatus(422);
+
+        // Test if we send correct data to product store endpoint, it will be ok
+        $product_store_input = [
+            "code"=>"P003",
+            "title"=>"Second product",
+            "description"=>"Description of second product"
+        ];
+        $response = $this->post('/api/products',$product_store_input);
+        $response->assertStatus(200);
+    }
+
 }
