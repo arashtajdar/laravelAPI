8614baea679bf60916d4b284b5f6f7f6b900d88f
arashtajdar <arashtajdar@gmail.com>
2022-04-13 12:26:23 +0200

    Create show method route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
 
 Route::post('/products',[ProductController::class,'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
