fb2f46529d733bca2a2be17e2d9bc5e128cd745c
arashtajdar <arashtajdar@gmail.com>
2022-04-14 00:30:07 +0200

    Reformat some parts

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -31,11 +30,12 @@ class ProductTest extends TestCase
         $productModel = Product::factory()->make();
         $this->product = $productModel->getAttributes();
     }
+
     public function test_product_functionalities()
     {
         $response = $this->get('/api/products');
         $response->assertStatus(200);
-    // test product store functionality
+        // test product store functionality
 
         // Test if we do not have any input for product store method, it will fail and respond with 422
         $response = $this->post('/api/products');
@@ -48,14 +48,14 @@ class ProductTest extends TestCase
         $this->assertDatabaseHas('products', [
             'code' => $this->product["code"]
         ]);
-    // test product show functionality
+        // test product show functionality
         $response = $this->get('/api/products/' . $this->id);
         $response->assertStatus(200);
-    // test product update functionality
+        // test product update functionality
         $response = $this->put('/api/products/' . $this->id, $this->product);
         // Todo : check if it is really updated or not
         $response->assertStatus(200);
-    // test product delete functionality
+        // test product delete functionality
         $response = $this->delete('/api/products/' . $this->id);
         // Todo : check if it is really deleted or not
         $response->assertStatus(200);
