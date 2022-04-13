8f1954ce63f8520ff42a1ab2fa5c3b65664b556f
arashtajdar <arashtajdar@gmail.com>
2022-04-13 12:32:08 +0200

    Create update method for ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -63,7 +63,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return Response(new ProductCollection($product),"200");
     }
 
     /**
