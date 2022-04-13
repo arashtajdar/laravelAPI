9dbb84a7d58d4f1188c60d2c938a5f21109ab2cb
arashtajdar <arashtajdar@gmail.com>
2022-04-13 11:39:17 +0200

    Create a collection for products to show

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+/**
+ * @property mixed code
+ * @property mixed title
+ * @property mixed description
+ * @property mixed created_at
+ */
+class ProductCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'Product code' => $this->code,
+            'Product title' => $this->title,
+            'Description of product' => $this->description,
+            'Creation date' => Carbon::parse($this->created_at)->toDateString()
+        ];
+    }
+}
