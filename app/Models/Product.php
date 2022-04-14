7cd23c78af952f50dad028fa065f2bf6f6935a8d
arashtajdar <arashtajdar@gmail.com>
2022-04-14 11:34:02 +0200

    Modify product model to add new relation to category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,14 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'id';
+
     protected $fillable = [
-        "code", "title", "description"
+        "code", "title", "description", "category_id"
     ];
 
     // Todo : use relations for category
+    public function category(){
+        return $this->hasOne(Category::class,"id","category_id");
+    }
 }
