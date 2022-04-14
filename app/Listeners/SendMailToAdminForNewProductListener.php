9f88baba912be46a7405ae41241121aa62b8b3ca
arashtajdar <arashtajdar@gmail.com>
2022-04-14 18:15:14 +0200

    Create Mail, event and listener for new product

---
+++ app/Listeners/SendMailToAdminForNewProductListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Mail\NewProductAddedMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendMailToAdminForNewProductListener implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $product_code = $event->product->code;
+        Mail::to("email@email.com")->send(new NewProductAddedMail($product_code));
+    }
+}
