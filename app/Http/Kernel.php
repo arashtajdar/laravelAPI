38f86465c9ae3d6742e62e1af5e5ed0331aaf7a3
a.tajdar@united-remote.com <arashtajdar@gmail.com>
2022-09-17 21:32:50 +0200

    Create CountProductsView middleware and register it

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'increase.view.count' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
