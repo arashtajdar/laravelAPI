b07868bd6be7247e627b7a0842bb7f61597d0445
arashtajdar <arashtajdar@gmail.com>
2023-01-29 15:29:47 +0100

    Remove products and categories

--- app/Http/Middleware/CountProductsViewMiddleware.php
+++ app/Http/Middleware/CountProductsViewMiddleware.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Product;
+use App\Models\Activity;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -17,7 +17,7 @@ class CountProductsViewMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $product = Product::with("category")->find($request->id);
+        $product = Activity::find($request->id);
         $product?->increment("view_count");
         return $next($request);
     }
