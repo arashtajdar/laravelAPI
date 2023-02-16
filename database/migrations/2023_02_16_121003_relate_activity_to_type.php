09e9d76ce3fb0165fab4df15724f178a0252d245
arashtajdar <arashtajdar@gmail.com>
2023-02-16 13:14:49 +0100

    Add migration for type model

---
+++ database/migrations/2023_02_16_121003_relate_activity_to_type.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropColumn('type');
+            $table->unsignedBigInteger('type_id')->index('activities_type_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropForeign('activities_type_id_foreign');
+            $table->dropColumn('type_id');
+            $table->string('type');
+        });
+    }
+};
