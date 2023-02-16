7d1004c6b1bcb95293d8179a0ab1ca5f7c036df7
arashtajdar <arashtajdar@gmail.com>
2023-02-16 13:22:55 +0100

    Rename PostActivityRequest

--- app/Http/Requests/StoreActivityRequest.php
+++ app/Http/Requests/StoreActivityRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class PostActivityRequest extends FormRequest
+class StoreActivityRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
