f5f7168192129887805fc70ea2a4de1069e777ba
arashtajdar <arashtajdar@gmail.com>
2023-02-16 12:52:45 +0100

    Refactor some parts

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\NewProductAddedEvent;
 use App\Http\Requests\PostActivityRequest;
-use App\Http\Requests\PostProductRequest;
 use App\Http\Resources\ActivityCollection;
-use App\Http\Resources\ProductCollection;
 use App\Models\Activity;
-use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,7 +19,7 @@ class ActivityController extends Controller
     /**
      * Save a new product into database.
      *
-     * @param Request $request
+     * @param PostActivityRequest $request
      * @return Response
      */
     public function store(PostActivityRequest $request): Response
--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static findOrFail($id)
+ * @method static create(mixed $validatedData)
+ */
 class Activity extends Model
 {
     use HasFactory;
